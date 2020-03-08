a8db3eee72e2db7959185fe2a56d64f83855d624
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-08 12:47:46 -0500

    set up Nasa component

---
+++ src/components/Nasa.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import NasaCard from "./NasaCard.js";
+
+function Nasa() {
+    const [article, setArticle] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.nasa.gov/planetary/apod?api_key=7pkGQ2TpQLg4kYbGovdSN3B5bFEgRkqcjo8hLOUE")
+            .then(response => {
+                setArticle(response.data);
+            })
+    }, []);
+    useEffect(() => {
+    }, [article])
+
+    return <NasaCard
+
+        date={article.data}
+        title={article.titel}
+        url={article.url}
+        explaination={article.explaination}
+
+    />
+}
+
+export default Nasa;
